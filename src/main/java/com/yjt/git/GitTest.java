8c861b89ee57862ce768a124238850a993da6133
yjt <1090784659@qq.com>
2021-11-17 22:02:03 +0800

    third commit

--- src/main/java/com/yjt/git/GitTest.java
+++ src/main/java/com/yjt/git/GitTest.java
@@ -2,6 +2,7 @@ package com.yjt.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.printf("", "hello git");
+        System.out.printf("hello git");
+        System.out.println("hello3");
     }
 }
