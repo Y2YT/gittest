13aea98b90cee762c4d68de223d169f08d410ab7
yjt <1090784659@qq.com>
2021-11-17 22:00:39 +0800

    second commit

--- src/main/java/com/yjt/git/GitTest.java
+++ src/main/java/com/yjt/git/GitTest.java
@@ -3,6 +3,5 @@ package com.yjt.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.printf("", "hello git");
-        System.out.println("hello git");
     }
 }
